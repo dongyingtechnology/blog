e3f6a9fead10fcaa2ad8e7b99acd2f1ceedc5255
dongyingtechnology <280356557@qq.com>
2018-03-06 09:03:25 +0800

    Site updated: 2018-03-06 09:03:23

---
+++ js/musicList.js
@@ -0,0 +1,39 @@
+//http://www.radio.net/genre/Hardstyle/
+var musicList = [
+	{
+		name: 'Q-Dance',
+		author: 'q-dance.com',
+		src: 'http://19993.live.streamtheworld.com/Q_DANCEAAC.aac',
+		cover: 'http://pic.58pic.com/58pic/15/08/86/49D58PICHuN_1024.png'
+    },
+	{
+        name : 'HardcorePower',
+        author : 'HardStyle',
+        src    : 'http://src.shoutcaststream.com:8022/;',
+		cover  : 'http://hard.fm/wp-content/uploads/2016/03/12822018_667884410019223_238553539_n.jpg',
+    },
+	{
+		name : 'RealHardstyle',
+		author : 'rhr.fm',
+		src : 'http://stream2.rhr.fm/',
+		cover : 'http://rhr.fm/data/img/menuhead.png'
+	},
+	{
+		name : 'Absolute Hardstyle Radio',
+		author : 'hardstyleradio.org',
+		src : 'http://streaming.radionomy.com/Absolute-Hardstyle-Radio?lang=zh-CN%2czh%3bq%3d0.8',
+		cover : 'http://www.hardstyleradio.org/audios/LOGONEW.png'
+	},
+	{
+		name : 'HardstyleFM',
+		author : 'hardstylefm.eu',
+		src : 'http://hardstylefm.shoutcaststream.com:8280/stream',
+		cover : 'http://www.hardstylefm.eu/v1/wp-content/uploads/2017/01/hardstyle-fm-zwart.png'
+	},
+	{
+		name : 'sunshine live - Hard',
+		author : 'sunshine-live.de',
+		src : 'http://sunshinelive.hoerradar.de/sunshinelive-hard-mp3-hq?sABC=59n393p1%230%236o54psq99478op01np9r495o8o579s65%23enqvbqr&amsparams=playerid:radiode;skey:1503892417',
+		cover : 'http://static.radio.net/images/broadcasts/a2/5a/21211/1/c175.png'
+	}
+];
